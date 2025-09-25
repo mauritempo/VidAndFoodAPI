94006db32fa80d28e9c42db7ff46cf9d78f8878f
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2025-09-25 11:37:34 -0300

    Allow cors (for use any endpoint)

--- WineAndFoodAPI/Program.cs
+++ WineAndFoodAPI/Program.cs
@@ -99,6 +99,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
