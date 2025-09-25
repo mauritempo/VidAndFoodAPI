54c338f725416c71ef985941fe79ae4bf4090774
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2025-09-25 11:46:59 -0300

    Healthy endpoint

---
+++ WineAndFoodAPI/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WineAndFoodAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+    }
+}
